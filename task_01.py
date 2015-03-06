2b24b835483b2c2c7225c063087c2e4a0a6a1e4c
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-06 22:01:53 +0000

    created task_01 set function and variables

---
+++ task_01.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Module that returns actual date"""
+import datetime
+
+CURDATE = None
+
+
+def get_current_date():
+    return datetime.date.today()
+
+
+if __name__=='__main__':
+    CURDATE = get_current_date()
