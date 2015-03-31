90c51dfce7d04072b4d220778fff8406787cd65e
modestocabrera <modesto.cabrera@spsmail.cuny.edu>
2015-03-31 04:47:08 +0000

    redoassgin

--- task_01.py
+++ task_01.py
@@ -7,9 +7,18 @@ CURDATE = None
 
 
 def get_current_date():
-    """Return the date today"""
-    return datetime.date.today()
+    """Return the date for today.
+    Arg:
+        None
 
+    Returns:
+        datetime.date.today() : Todays date.
+
+    Examples:
+        >>> get_current_date()
+        datetime.date(2015, 3, 31)
+    """
+    return datetime.date.today()
 
 if __name__ == '__main__':
     CURDATE = get_current_date()
